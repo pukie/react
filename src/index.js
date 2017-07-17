2c8dc5099ef065786c2d783f9815c104937d1a39
Łukasz Komajda <lukasz.komajda@atena.pl>
2017-07-17 12:38:49 +0200

    dodanie gridu z podziałem na drużyny

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Grid, Row, Col } from 'react-bootstrap';
 // class Square extends React.Component {
 //
 //     render(){
@@ -12,61 +14,38 @@ import './index.css';
 //         );
 //     }
 // }
-function Square(props){
+function Team(props){
     return (
-        <button className="square" onClick={props.onClick}>
+
+        <div className="team" onClick={props.onClick}>
             {props.value}
-        </button>
+        </div>
+
     )
 }
 
-function calculateWinner(squares){
-    const lines =[
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
+
 class Board extends React.Component {
 
     renderSquare(i){
-        return <Square value={this.props.squares[i]}
+        return <Team value={this.props.squares[i]}
                        onClick={() => this.props.onClick(i)}/>;
     }
 
     render(){
 
         return (
-            <div>
+            <Grid fluid={true}>
+                <Row className="show-grid" style={{height:"50vh"}}>
+                    <Col xs={6} md={6}> {this.renderSquare(0)}</Col>
+                    <Col xs={6} md={6}> {this.renderSquare(1)}</Col>
+                </Row>
 
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+                <Row className="show-grid" style={{height:"50vh"}}>
+                    <Col xs={6} md={6}> {this.renderSquare(2)}</Col>
+                    <Col xs={6} md={6}> {this.renderSquare(3)}</Col>
+                </Row>
+            </Grid>
         );
     }
 }
@@ -86,9 +65,7 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
+
         squares[i]= this.state.x ? 'X': 'O';
         this.setState({
             history: history.concat([{
@@ -108,30 +85,14 @@ class Game extends React.Component {
     render(){
         const history = this.state.history;
         const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
 
-        const moves = history.map((step,move) => {
-            const desc = move ? 'ruch #'+move : 'Start gry';
-            return (
-                <li key={move}>
-                    <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
-                </li>
-            )
-        });
+
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.x ? 'X' : 'O');
-        }
+
         return (
             <div className="game">
                 <div className="game-board">
                     <Board squares={current.squares} onClick={(i) => this.handleClick(i) }/>
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
                 </div>
             </div>
         )
