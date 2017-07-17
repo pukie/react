dc77addb000f4b87debdeb6528bbad5a3da01b7a
Łukasz Komajda <lukasz.komajda@atena.pl>
2017-07-17 15:56:09 +0200

    ustawianie divów z odpowiedziami

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import { Grid, Row, Col } from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 // class Square extends React.Component {
 //
 //     render(){
@@ -14,11 +14,49 @@ import { Grid, Row, Col } from 'react-bootstrap';
 //         );
 //     }
 // }
-function Team(props){
+function Team(props) {
     return (
 
         <div className="team" onClick={props.onClick}>
-            {props.value}
+
+            <div className="team_info">
+                {props.value}
+            </div>
+            <div className="answers">
+                <Row className="show-grid" style={{height: "50px"}}>
+                    <Col xs={6} md={6}>
+                        <div style={{display: "flex"}}>
+                            <div className="triangle-left"></div>
+                            <div className="answer">aaa</div>
+                            <div className="triangle-right"></div>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={6}>
+                        <div style={{display: "flex"}}>
+                            <div className="triangle-left"></div>
+                            <div className="answer">aaa</div>
+                            <div className="triangle-right"></div>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid" style={{height: "50px"}}>
+                    <Col xs={6} md={6}>
+                        <div style={{display: "flex"}}>
+                            <div className="triangle-left"></div>
+                            <div className="answer">aaa</div>
+                            <div className="triangle-right"></div>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={6}>
+                        <div style={{display: "flex"}}>
+                            <div className="triangle-left"></div>
+                            <div className="answer">aaa</div>
+                            <div className="triangle-right"></div>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
         </div>
 
     )
@@ -27,21 +65,21 @@ function Team(props){
 
 class Board extends React.Component {
 
-    renderSquare(i){
+    renderSquare(i) {
         return <Team value={this.props.squares[i]}
-                       onClick={() => this.props.onClick(i)}/>;
+                     onClick={() => this.props.onClick(i)}/>;
     }
 
-    render(){
+    render() {
 
         return (
             <Grid fluid={true}>
-                <Row className="show-grid" style={{height:"50vh"}}>
+                <Row className="show-grid" style={{height: "50vh"}}>
                     <Col xs={6} md={6}> {this.renderSquare(0)}</Col>
                     <Col xs={6} md={6}> {this.renderSquare(1)}</Col>
                 </Row>
 
-                <Row className="show-grid" style={{height:"50vh"}}>
+                <Row className="show-grid" style={{height: "50vh"}}>
                     <Col xs={6} md={6}> {this.renderSquare(2)}</Col>
                     <Col xs={6} md={6}> {this.renderSquare(3)}</Col>
                 </Row>
@@ -51,22 +89,23 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            history:[{
-                squares:Array(9).fill(null),
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
             }],
-            x:true,
-            stepNumber:0
+            x: true,
+            stepNumber: 0
         }
     }
-    handleClick(i){
+
+    handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
-        squares[i]= this.state.x ? 'X': 'O';
+        squares[i] = this.state.x ? 'X' : 'O';
         this.setState({
             history: history.concat([{
                 squares: squares,
@@ -75,14 +114,15 @@ class Game extends React.Component {
             stepNumber: history.length,
         });
     }
-    jumpTo(step){
+
+    jumpTo(step) {
         this.setState({
-            stepNumber:step,
+            stepNumber: step,
             x: (step % 2) === 0
         })
     }
 
-    render(){
+    render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
 
